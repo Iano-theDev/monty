5f566004ef06bb0343ab0f0d8460ebd2eaeec01f
codivily <codivily@gmail.com>
2022-06-18 13:59:46 -0700

    op_pint.c error message typo fixed

--- op_pint.c
+++ op_pint.c
@@ -12,7 +12,7 @@ void op_pint(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't point, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
