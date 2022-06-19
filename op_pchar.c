18b04e15f7b761de2b73bf809f59bb33c95510e4
codivily <codivily@gmail.com>
2022-06-19 01:33:40 -0700

    op_pchar.c update

--- op_pchar.c
+++ op_pchar.c
@@ -17,7 +17,7 @@ void op_pchar(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if (!((*stack)->n >= 0 && (*stack)->n <= 127))
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		free_stack(stack);
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
