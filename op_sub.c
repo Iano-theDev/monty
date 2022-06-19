3576184d436116fc2ce5bf1a269ec2c3b8db112d
Iano-theDev <sinceianmike@gmail.com>
2022-06-19 11:00:07 +0300

    Betty checks for op_sub.c rectified

--- op_sub.c
+++ op_sub.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * op_sub - subtructs the top two elements of the stack
  * @stack: the stack
@@ -10,13 +9,13 @@
  */
 void op_sub(stack_t **stack, unsigned int line_number)
 {
-        if (!(*stack) || !(*stack)->next)
-        {
-                free_stack(stack);
-                fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	if (!(*stack) || !(*stack)->next)
+	{
+		free_stack(stack);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        (*stack)->next->n -= (*stack)->n;
-        free_stack_t(stack_pop(stack));
+	(*stack)->next->n -= (*stack)->n;
+	free_stack_t(stack_pop(stack));
 }
