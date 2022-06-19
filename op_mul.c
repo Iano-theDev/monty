1c9820b070036e7c11f314c6d386dbcdd90c0a19
Iano-theDev <sinceianmike@gmail.com>
2022-06-19 11:05:12 +0300

    Correct betty checks for op_mul.c

--- op_mul.c
+++ op_mul.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * op_add - multiplies the second top element of the stack with the top element
+ * op_mul - multiplies the second top element of the stack with the top element
  * of  the stack.
  * @stack: The stack
  * @line_number: An integer
@@ -11,13 +11,13 @@
  */
 void op_mul(stack_t **stack, unsigned int line_number)
 {
-        if (!(*stack) || !(*stack)->next)
-        {
-                free_stack(stack);
-                fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+	if (!(*stack) || !(*stack)->next)
+	{
+		free_stack(stack);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        (*stack)->next->n *= (*stack)->n;
-        free_stack_t(stack_pop(stack));
+	(*stack)->next->n *= (*stack)->n;
+	free_stack_t(stack_pop(stack));
 }
